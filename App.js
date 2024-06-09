afc1b7c09726bdb671f1f242f6a083ee848452fe
Ariano2 <aryanramjasian@gmail.com>
2024-06-09 17:36:00 +0530

    Add a header from scratch
    Using react components and JSX a header was created

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-const div=React.createElement('div',{id:'parent'},
-React.createElement('div',{id:'child'},
-[React.createElement('h1',{className:'heading'},"I'm the first child"),
-React.createElement('h1',{className:'heading'},"I'm the second child")]
-));
-const root=ReactDOM.createRoot(document.getElementById('container'));
-root.render(div);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const HeaderComponent = () => (
+  <header className="header">
+    <img
+      src="https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_640.png"
+      alt="Company Logo"
+      width="100px"
+      height="80px"
+      crossOrigin="anonymous"
+    ></img>
+    <form>
+      <input type="text" placeholder="search here"></input>
+    </form>
+    <img
+      src="https://static.vecteezy.com/system/resources/thumbnails/005/545/335/small/user-sign-icon-person-symbol-human-avatar-isolated-on-white-backogrund-vector.jpg"
+      alt="user icon"
+      width="100px"
+    ></img>
+  </header>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeaderComponent />);
